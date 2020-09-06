b8ebf75d91dde997adffdf7c925491cf91092dcf
PlatinumMaster <l33tgamer@outlook.com>
2020-09-06 16:05:52 -0400

    Update usage.

--- Program.cs
+++ Program.cs
@@ -24,6 +24,6 @@ namespace BeaterText
             }
         }
 
-        private static string usage = $"Bah humbug";
+        private static string usage = "BeaterText: Usage\n" + "To decompile: BeaterText -d <input> <output>\n" + "To compile: BeaterText -d <input> <output>";
     }
 }
